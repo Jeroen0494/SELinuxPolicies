abbc96e14cda3bf29c40ff87ad311d7b005e52e0
Jeroen Rijken <Jeroen.Rijken@gmail.com>
2015-10-12 14:58:59 +0200

    Add container program + some cleanups

--- container/process2/process2t1.c
+++ container/process2/process2t1.c
@@ -4,13 +4,13 @@ int main() {
 	char text[20];
 	printf("Please type something to continue... \n");
 	scanf("%s", text);
-	fp = fopen("/opt/container/process2/data/process1_direct.txt","w");
+	fp = fopen("/opt/container/process2/data/process2_direct.txt","w");
 	if(fp == 0) {
 		printf("Error! \n");
 		return 1;
 	}
  
-	fprintf(fp,"This file was created by process 1");
+	fprintf(fp,"This file was created by process 2");
 	fclose(fp);
 	return 0;
 }
